51f1a1285e40d469b190a27972759a56d19236f6
Vignesh Shanmugam <vignesh.shanmugam@flipkart.com>
2015-03-21 20:38:14 +0530

    Fixed Mx Record Bug

--- static/app.bundle.js
+++ static/app.bundle.js
@@ -152,12 +152,8 @@
 
 	var RECORD_NAMES = ["IPv4", "IPv6", "Canonical Name", "Name Servers", "Mail Exchange", "Service", "Start of authority"];
 
-	function createSubComponents(record, col) {
-		var subComp,
-		    keyName,
-		    valName,
-		    r,
-		    subTable = document.createElement("table");
+	function createObjComponents(record, subTable, col) {
+		var subComp, keyName, valName, r;
 		for (r in record) {
 			if (record.hasOwnProperty(r)) {
 				subComp = subTable.insertRow();
@@ -168,18 +164,34 @@
 				valName.innerHTML = record[r];
 			}
 		}
-		subTable.className = "sub-table";
+		col.appendChild(subTable);
+	}
+
+	function createArrayComponent(record, subTable, col) {
+		var subComp, valName, r;
+
+		for (r = 0; r < record.length; r++) {
+			subComp = subTable.insertRow();
+			valName = subComp.insertCell();
+			valName.innerHTML = record[r];
+		}
 		col.appendChild(subTable);
 	}
 
 	module.exports = function (table, record, recordNo) {
 		var row = table.insertRow(),
 		    col1 = row.insertCell(),
-		    col2 = row.insertCell();
+		    col2 = row.insertCell(),
+		    subTable = document.createElement("table");
+
 		col1.innerHTML = RECORD_NAMES[recordNo];
 		if (record) {
 			if (Object.prototype.toString.call(record) === "[object Object]") {
-				createSubComponents(record, col2);
+				subTable.className = "sub-table";
+				createObjComponents(record, subTable, col2);
+			} else if (Object.prototype.toString.call(record) === "[object Array]") {
+				subTable.className = "sub-table";
+				createArrayComponent(record, subTable, col2);
 			} else {
 				col2.innerHTML = record;
 			}
